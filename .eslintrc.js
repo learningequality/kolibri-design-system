74d2d03b0952d109cc80328df544eb844eeb0e87
Devon Rueckner <devon@learningequality.org>
2020-01-31 16:36:05 -0800

    update dependencies
    waiting for next version of kolibri-tools to fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,7 @@
-module.exports = require('kolibri-tools/.eslintrc');
+const esLintConfig = require('kolibri-tools/.eslintrc');
+
+// Vuetify's helper attributes use hyphens and they would
+// not be recognized if auto-formatted to camel case
+esLintConfig.rules['vue/attribute-hyphenation'] = 0;
+
+module.exports = esLintConfig;
