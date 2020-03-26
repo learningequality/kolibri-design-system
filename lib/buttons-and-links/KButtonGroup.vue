68d87bd8eaef44bd168e4ae517f7842ed96d9cc1
Jacob Pierce <w.jacobpierce@gmail.com>
2020-03-25 17:50:55 -0700

    Add KButtonGroup - remove base button margin

---
+++ lib/buttons-and-links/KButtonGroup.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <span class="button-group">
+    <slot></slot>
+  </span>
+
+</template>
+
+
+<script>
+
+export default {
+  name: "KButtonGroup",
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+  .button-group {
+    overflow: none;
+  }
+  /* KButton can produce <a> or <button> tags */
+  .button-group > a,
+  .button-group > button {
+    margin: 8px;
+  }
+</style>
