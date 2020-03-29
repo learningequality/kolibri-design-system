91fcb921aeca948e9b61cb89351f9a465b5992b8
Devon Rueckner <devon@learningequality.org>
2020-03-29 03:31:45 -0700

    sample files

---
+++ lib/Hello.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+    <p>Hello world.</p>
+    <ul>
+      <li>
+        number: {{ number }}
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+
+<script>
+
+  /**
+   * What do we have here...
+   */
+  export default {
+    name: 'Hello',
+    props: {
+      /**
+       * The color for the button.
+       */
+      number: {
+        type: Number,
+        required: true,
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
