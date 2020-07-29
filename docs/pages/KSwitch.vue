7965563f0062163b11285407476ffc5d3ff334b5
Devon Rueckner <devon@learningequality.org>
2020-07-28 18:10:29 -0700

    links

--- docs/pages/KSwitch.vue
+++ docs/pages/KSwitch.vue
@@ -4,15 +4,17 @@
 
     <DocsPageSection title="Overview" anchor="#overview">
       <DocsShow>
-        <KSwitch 
+        <KSwitch
           :value="true"
         />
       </DocsShow>
       <p>
-        A switch toggle is used to select and execute an action instantly, in real time. A switch is toggled successfully when the switch thumb slides to the other side of the track upon click or press. 
+        A switch toggle is used to select and execute an action instantly, in real time. A switch is toggled successfully when the switch thumb slides to the other side of the track upon click or press.
       </p>
       <p>
-        For selections that require a confirmation to execute and are housed in a form, use <code>KCheckbox</code> component instead.
+        For selections that require a confirmation to execute and are housed in a form, use <DocsInternalLink href="/KCheckbox" code>
+          KCheckbox
+        </DocsInternalLink> component instead.
       </p>
     </DocsPageSection>
 
@@ -29,10 +31,30 @@
 
     <DocsPageSection title="Specifications" anchor="#specifications">
       <ul>
-        <li>Thumb ON color: <code>palette.green.v_500</code></li>
-        <li>Track ON color: <code>palette.green.v_200</code></li>
-        <li>Thumb OFF color: <code>palette.grey.v_400</code></li>
-        <li>Track OFF color: <code>palette.grey.v_50</code></li>
+        <li>
+          Thumb ON color:
+          <DocsInternalLink href="/colors#palette-green-v_500" code>
+            palette.green.v_500
+          </DocsInternalLink>
+        </li>
+        <li>
+          Track ON color:
+          <DocsInternalLink href="/colors#palette-green-v_200" code>
+            palette.green.v_200
+          </DocsInternalLink>
+        </li>
+        <li>
+          Thumb OFF color:
+          <DocsInternalLink href="/colors#palette-grey-v_400" code>
+            palette.grey.v_400
+          </DocsInternalLink>
+        </li>
+        <li>
+          Track OFF color:
+          <DocsInternalLink href="/colors#palette-grey-v_50" code>
+            palette.grey.v_50
+          </DocsInternalLink>
+        </li>
       </ul>
     </DocsPageSection>
 
