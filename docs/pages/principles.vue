d6ae44d71c258eba443cbea9266c62840139c682
Jessica Aceret <jessica@learningequality.org>
2020-08-31 17:02:35 -0700

    update styling on design principles

--- docs/pages/principles.vue
+++ docs/pages/principles.vue
@@ -8,7 +8,9 @@
       <p>Finally, these principles are a work in progress. The intent is that this is a living set of principles that will evolve to better serve the design team, the product process, and our users.</p>
     </DocsPageSection>
     <DocsPageSection title="Equitable access" anchor="#equitable">
-      <p>Equity is at the heart of Learning Equality’s mission. Our products address barriers of access to educational opportunity that are faced by marginalized populations.</p>
+      <p class="description">
+        Equity is at the heart of Learning Equality’s mission. Our products address barriers of access to educational opportunity that are faced by marginalized populations.
+      </p>
       <ul>
         <li>Aim to be as functional as possible in situations with unstable or no internet access</li>
         <li>Support old, legacy, and low-powered devices</li>
@@ -17,7 +19,9 @@
       </ul>
     </DocsPageSection>
     <DocsPageSection title="Effective learning" anchor="#effective">
-      <p>The learning process can sometimes feel tiring and challenging. Learning can be far more effective by keeping people motivated and engaged. We support not only concept and skill proficiency, but also the ability to continue learning in the future.</p>
+      <p class="description">
+        The learning process can sometimes feel tiring and challenging. Learning can be far more effective by keeping people motivated and engaged. We support not only concept and skill proficiency, but also the ability to continue learning in the future.
+      </p>
       <ul>
         <li>Support learner progression as people master and engage content more deeply</li>
         <li>Don’t interrupt the learning process unless doing so is conducive to learning</li>
@@ -26,7 +30,9 @@
       </ul>
     </DocsPageSection>
     <DocsPageSection title="Community ownership" anchor="#community">
-      <p>Communities understand their own needs best. Our tools enable them to have ownership over how their goals, educational resources, and identities are represented. We understand that one size doesn’t fit all, so we serve the unique needs of our communities in these ways:</p>
+      <p class="description">
+        Communities understand their own needs best. Our tools enable them to have ownership over how their goals, educational resources, and identities are represented. We understand that one size doesn’t fit all, so we serve the unique needs of our communities in these ways:
+      </p>
       <ul>
         <li>Make it easy to share and collaborate online and offline</li>
         <li>Support purposeful content editing for pedagogy and localization</li>
@@ -35,7 +41,9 @@
       </ul>
     </DocsPageSection>
     <DocsPageSection title="Consistent experiences" anchor="#consistent">
-      <p>Consistency removes friction with the product, allowing people to focus on what they’re trying to get done. Each function of our products should feel empowering and familiar with the rest of the product ecosystem.</p>
+      <p class="description">
+        Consistency removes friction with the product, allowing people to focus on what they’re trying to get done. Each function of our products should feel empowering and familiar with the rest of the product ecosystem.
+      </p>
       <ul>
         <li>Lean on established patterns and components that live in our design system</li>
         <li>Introduce each new experience in a clear way that leaves no room for guesswork</li>
@@ -48,4 +56,14 @@
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+  .description {
+    font-size: 18px;
+  }
+  li {
+    font-size: 14px;
+    color: #616161;
+  }
+
+</style>
