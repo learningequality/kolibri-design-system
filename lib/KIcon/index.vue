6b98c03dddbb23170e98d7d11afa942e36be2bd8
Jacob Pierce <w.jacobpierce@gmail.com>
2020-09-22 08:54:46 -0700

    useful errors on missing icons

--- lib/KIcon/index.vue
+++ lib/KIcon/index.vue
@@ -23,7 +23,12 @@
         type: String,
         required: true,
         validator(value) {
-          return Object.keys(KolibriIcons).includes(value);
+          if(Object.keys(KolibriIcons).includes(value)) {
+            return true;
+          } else {
+            console.error(`KIcon: No icon defined for token: ${value}`);
+            return false;
+          }
         },
       },
       /**
