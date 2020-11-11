dda827a97be1021112d74d79290c2c8839d48f5d
Devon Rueckner <devon.rueckner@gmail.com>
2020-11-10 16:58:08 -0800

    update versions doc

--- docs/pages/versions.vue
+++ docs/pages/versions.vue
@@ -11,17 +11,17 @@
     <DocsPageSection title="Version 0.2" anchor="#0.2">
       <h3>0.2.1</h3>
       <p>
-        info
+        Fixed regression related to all external links opening in new tabs - <DocsGithubLink pull="93" />
       </p>
       <h3>0.2.0</h3>
       <p>
-        info
+        This was the first release of the Design System, with documentation written in a Nuxt-based statically-generated site. The focus was on migrating components out of the Kolibri and making them reusable in a shared component library.
       </p>
     </DocsPageSection>
 
     <DocsPageSection title="Version 0.1" anchor="#0.1">
       <p>
-        The design system here is based on a set of internal Kolibri components and the Kolibri Style Guide, which was first introduced into the Kolibri code base <DocsExternalLink text="in version 0.6" href="https://github.com/learningequality/kolibri/tree/release-v0.6.x/kolibri/plugins/style_guide" />. This remained until <DocsExternalLink text="version 0.13" href="https://github.com/learningequality/kolibri/tree/release-v0.13.x/kolibri/plugins/style_guide" /> after which the content was migrated here.
+        The design system was originally based on a set of internal Kolibri components and their use as documented in the Kolibri Style Guide, which was first introduced into the Kolibri code base <DocsExternalLink text="in version 0.6" href="https://github.com/learningequality/kolibri/tree/release-v0.6.x/kolibri/plugins/style_guide" />. This remained until <DocsExternalLink text="version 0.13" href="https://github.com/learningequality/kolibri/tree/release-v0.13.x/kolibri/plugins/style_guide" /> after which the content was migrated here.
       </p>
     </DocsPageSection>
 
