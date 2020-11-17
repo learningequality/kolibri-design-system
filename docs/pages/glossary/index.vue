f828f8eaf6404e8b327994912bfb183eca1b6f68
Devon Rueckner <devon.rueckner@gmail.com>
2020-11-17 13:46:32 -0800

    adding context

--- docs/pages/glossary/index.vue
+++ docs/pages/glossary/index.vue
@@ -3,6 +3,15 @@
   <DocsPageTemplate>
 
     <DocsPageSection title="Overview" anchor="#overview">
+      <p>
+        Certain words have special meanings in the context of the Kolibri ecosystem which might be different or more specific than when used in general language. Additionally, many of the terms have been carefully chosen from among alternatives, and it's important to consistently use this terminology across the product ecosystem.
+      </p>
+      <p>
+        The glossary is stored in the <DocsExternalLink text="the TBX-Glossary format" href="http://www.ttt.org/tbxg/" /> format, and should be uploaded to all Crowdin projects so that translators have this context.
+      </p>
+    </DocsPageSection>
+
+    <DocsPageSection title="Terms" anchor="#terms">
       <pre>{{ terms }}</pre>
     </DocsPageSection>
 
