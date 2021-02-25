f20ca13094da9a1ebe1ed9f950aec0fa07edc47f
sairina <sairina.merinotsui@gmail.com>
2021-02-25 14:48:34 -0800

    Match KRouterLink to KExternalLink styling

--- lib/buttons-and-links/KRouterLink.vue
+++ lib/buttons-and-links/KRouterLink.vue
@@ -6,32 +6,27 @@
     :to="to"
     :replace="replace"
     dir="auto"
+    @mouseenter="hovering = true"
+    @mouseleave="hovering = false"
   >
-    <KLabeledIcon
-      :maxWidth="maxWidth"
-      @mouseenter="hovering = true"
-      @mouseleave="hovering = false"
-    >
+
+    <slot name="icon">
       <KIcon
         v-if="icon"
-        slot="icon"
         :icon="icon"
-        style="top: 2px;"
+        style="top: 4px;"
         :color="hovering ? $themeTokens.primaryDark : $themeTokens.primary"
       />
-
-      <span class="link-text">{{ text }}</span>
-
+    </slot>
+    <span class="link-text" :style="spanStyle">{{ text }}</span>
+    <slot name="iconAfter">
       <KIcon
         v-if="iconAfter"
-        slot="iconAfter"
         :icon="iconAfter"
-        style="top: 2px;"
+        style="top: 4px;"
         :color="hovering ? $themeTokens.primaryDark : $themeTokens.primary"
       />
-
-
-    </KLabeledIcon>
+    </slot>
   </router-link>
 
 </template>
@@ -76,18 +71,41 @@
         type: String,
         default: null,
       },
-      /**
-       * Set a max width for content
-       */
-      maxWidth: {
-        type: String,
-        default: '100%',
-      },
     },
     data() {
       return {
         hovering: false,
       };
+    },
+    computed: {
+      /**
+       * If link opens in new tab or if icon is provided, add 8px margin between the icon and the text
+       */
+      spanStyle() {
+        let styles = {};
+        if (this.icon) {
+          if (this.isRtl) {
+            // If RTL-language, but English link, displays correct margins
+            styles['marginRight'] = '8px';
+            // Checks to see if link for new tab is in same dir as page lang
+            if (this.text !== this.href) {
+              styles['marginRight'] = '0px';
+              styles['marginLeft'] = '8px';
+            }
+          } else {
+            if (this.text === this.href) {
+              styles['marginRight'] = '8px';
+            } else {
+              styles['marginLeft'] = '8px';
+            }
+          }
+        }
+
+        if (this.iconAfter) {
+          styles['marginRight'] = '8px';
+        }
+        return { ...styles };
+      },
     },
   };
 
