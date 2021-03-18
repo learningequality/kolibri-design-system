bf4fbd1fa57550ae12b39215d767e8eb41bed524
Jacob Pierce <w.jacobpierce@gmail.com>
2021-03-18 12:33:31 -0700

    Use `primaryDark` token for all places where `primary` was used
    also changes `themeTokens.error` to use the `red.v_800`
    that `themeTokens.incorrect` does to improve a11y and
    because it wasn't chosen with any particular purpose
    https://github.com/learningequality/kolibri-design-system/pull/139#issuecomment-802203343

--- lib/keen/UiTextbox.vue
+++ lib/keen/UiTextbox.vue
@@ -36,7 +36,7 @@
           :readonly="readonly"
           :required="required"
           :step="stepValue"
-          :style="isActive ? { borderBottomColor: $themeTokens.primary } : {}"
+          :style="isActive ? { borderBottomColor: $themeTokens.primaryDark } : {}"
           :tabindex="tabindex"
           :type="type"
           :value="value"
@@ -67,7 +67,7 @@
           :required="required"
 
           :rows="rows"
-          :style="isActive ? { borderBottomColor: $themeTokens.primary } : {}"
+          :style="isActive ? { borderBottomColor: $themeTokens.primaryDark } : {}"
 
           :tabindex="tabindex"
 
@@ -499,7 +499,6 @@
     width: 100%;
     padding: 4px 0 0 0;
     margin: 0;
-    background: #e9e9e9;
     border-radius: 4px 4px 0 0;
   }
 
