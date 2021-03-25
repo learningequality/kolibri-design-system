875f7c413837cd8500acd21a4a96ffd18f42951e
Michaela <michaela@learningequality.org>
2021-03-25 09:32:05 +0100

    Improve naming

--- docs/plugins/load-lib-components.js
+++ docs/plugins/load-lib-components.js
@@ -5,6 +5,6 @@ import trackInputModality from '~~/lib/styles/trackInputModality';
 // `KThemePlugin` dependency needed for outline style when
 // navigating between KDS components with keyboard
 // See `KThemePlugin` `$coreOutline` and `globalThemeState.inputModality`
-trackInputModality({ disableDefaultFocusRing: false });
+trackInputModality({ disableFocusRingByDefault: false });
 
 Vue.use(KThemePlugin);
