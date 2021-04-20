0ec8b531bbaed858b762ae4d1bc8aa232570426c
Devon Rueckner <devon.rueckner@gmail.com>
2021-04-20 16:49:15 -0700

    add support for ignored events

--- docs/common/DocsPageTemplate/jsdocs/EventsTable.vue
+++ docs/common/DocsPageTemplate/jsdocs/EventsTable.vue
@@ -10,7 +10,7 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(event, i) in api" :key="i">
+      <tr v-for="(event, i) in publicApi" :key="i">
         <td class="first-col">
           <code>{{ event.name }}</code>
           <DocsAnchorTarget :anchor="`#event:${event.name}`" />
@@ -34,6 +34,16 @@
       api: {
         type: Array,
         required: true,
+      },
+    },
+    computed: {
+      publicApi() {
+        return this.api.filter(docItem => {
+          if (!docItem['tags']) {
+            return true;
+          }
+          return !docItem['tags'].some(tagObj => tagObj['title'] === 'ignore');
+        });
       },
     },
   };
