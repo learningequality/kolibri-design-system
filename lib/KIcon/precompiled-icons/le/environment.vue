fb4f6ecbe2a6cf8e9177a2c3da3cf6a36ed4895c
Devon Rueckner <devon.rueckner@gmail.com>
2021-10-26 13:58:56 -0400

    add new custom icons

---
+++ lib/KIcon/precompiled-icons/le/environment.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M28.333 13.333c-15 3.334-18.5 13.617-21.966 22.234l3.15 1.1 1.583-3.834c.8.284 1.633.5 2.233.5C31.667 33.333 36.667 5 36.667 5 35 8.333 23.333 8.75 15 10.417 6.667 12.083 3.333 19.167 3.333 22.5s2.917 6.25 2.917 6.25c5.417-15.417 22.083-15.417 22.083-15.417z" fill="#000"/></svg>
+
+</template>
+
+
+<script>
+
+  export default {"name":"icon-44282db56a03196ec242a8c59b32e90a"}
+
+</script>
