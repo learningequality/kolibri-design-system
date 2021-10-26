808b7cb1cbacc659664c9678ce5bd78b60666c17
Devon Rueckner <devon.rueckner@gmail.com>
2021-10-26 15:12:38 -0400

    fix up sizes

--- lib/KIcon/precompiled-icons/le/currentEvents.vue
+++ lib/KIcon/precompiled-icons/le/currentEvents.vue
@@ -1,12 +1,12 @@
 <template>
 
-  <svg viewBox="0 0 24 24" role="presentation" focusable="false" width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M33.333 8.333v23.334H6.667V8.333h26.666zm0-3.333H6.667a3.322 3.322 0 00-3.334 3.333v23.334A3.322 3.322 0 006.667 35h26.666a3.322 3.322 0 003.334-3.333V8.333A3.322 3.322 0 0033.333 5zM30 25H10v3.333h20V25zM16.667 11.667H10v10h6.667v-10zM20 15h10v-3.333H20V15zm10 3.333H20v3.334h10v-3.334z" fill="#000"/></svg>
+  <svg role="presentation" focusable="false" width="24" height="24" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M33.333 8.333v23.334H6.667V8.333h26.666zm0-3.333H6.667a3.322 3.322 0 00-3.334 3.333v23.334A3.322 3.322 0 006.667 35h26.666a3.322 3.322 0 003.334-3.333V8.333A3.322 3.322 0 0033.333 5zM30 25H10v3.333h20V25zM16.667 11.667H10v10h6.667v-10zM20 15h10v-3.333H20V15zm10 3.333H20v3.334h10v-3.334z" fill="#000"/></svg>
 
 </template>
 
 
 <script>
 
-  export default {"name":"icon-5096b1c645566e2d4d3a5c24757de57f"}
+  export default {"name":"icon-2774bbe3c0b198f769e0201c353eb62a"}
 
 </script>
