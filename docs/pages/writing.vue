3224436feba48a76b5ed3c558b073ea282bccc55
Devon Rueckner <devon.rueckner@gmail.com>
2021-12-13 18:37:05 -0500

    'writing' page updates
    fixes https://github.com/learningequality/kolibri-design-system/issues/266

--- docs/pages/writing.vue
+++ docs/pages/writing.vue
@@ -9,6 +9,7 @@
     </DocsPageSection>
 
     <DocsPageSection title="Language &amp; tone" anchor="#language">
+      <p>Most of these tips are the same as Material's guidelines:</p>
       <ul>
         <li>Address the user as 'you', and avoid using 'we'</li>
         <li>Use the <DocsExternalLink href="https://en.wikipedia.org/wiki/Singular_they" text="gender-neutral singular" /> 'they'</li>
@@ -21,18 +22,16 @@
         <li>Reveal detail as needed</li>
         <li>Be positive</li>
         <li>Communicate only essential details</li>
-        <li>
-          Avoid creating new Proper Nouns in casual messages. For example,
-          "You are a coach", not "You are a Coach"
-        </li>
       </ul>
+      <p>
+        In Kolibri products, avoid creating new Proper Nouns in casual messages. For example, "You are a coach", not "You are a Coach". <DocsInternalLink href="/glossary" text="See also the Glossary page" /> which outlines terms that have a specific meaning in the Kolibri product ecosystem.
+      </p>
     </DocsPageSection>
 
     <DocsPageSection title="Punctuation" anchor="#punctuation">
       <ul>
-        <li>Use sentence-style caps</li>
+        <li>Use "Sentence case" capitalization for titles, not "Title Case" capitalization</li>
         <li>Skip periods and unnecessary punctuation</li>
-        <li>Skip colons after labels</li>
         <li>Use contractions when they make a sentence easier to understand</li>
         <li>Avoid exclamation marks</li>
         <li>Always wrap user-generated text in 'single quotes'</li>
@@ -44,14 +43,23 @@
         All user-facing text and iconography needs to be internationalized. For text strings, numbers, dates, and times, this means "wrapping" the messages such that they can be mapped to messages in other languages using the <DocsExternalLink href="https://formatjs.io/guides/message-syntax/" text="ICU message syntax" />. See our <DocsExternalLink href="https://kolibri-dev.readthedocs.io/en/develop/i18n.html" text="developer docs" /> for more information.
       </p>
       <p>
-        Icons and phrases that might not be globally understood should be avoided. Also, beware of using terms like 'left' and 'right' in RTL languages.
+        Phrases that might not be globally understood should be avoided because they will be cause issues with translation.
+      </p>
+      <p>
+        Note that currently our i18n framework does not support formatting such as bold or italics within a translated string.
       </p>
       <p>
         The built-in components in this guide support right-to-left languages out of the box. If you design or build custom components, these need to be fully tested using an RTL language. We use a <DocsExternalLink href="https://www.npmjs.com/package/webpack-rtl-plugin" text="webpack plugin" /> to automatically flip most of the CSS, and the global <code>isRtl</code> attribute on all Vue components can be used to customize behavior if necessary.
       </p>
       <p>
-        Note that currently our i18n framework does not support formatting such as bold or italics within a translated string.
+        To properly support right-to-left languages and bi-directional text in custom components, follow the following guidelines:
       </p>
+      <ul>
+        <li>No displayed user data or content should have <code>right</code> or <code>left</code> set for the <code>text-align</code> property (<code>center</code> is OK)</li>
+        <li>All displayed user data and content should have <code>dir="auto"</code></li>
+        <li><code>dir="auto"</code> should not generally be set on 'wrapper' nodes - it should be as close to the text as possible</li>
+        <li>Translated "app text" does not generally need to have <code>dir="auto"</code></li>
+      </ul>
     </DocsPageSection>
 
   </DocsPageTemplate>
