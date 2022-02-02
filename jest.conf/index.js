ed63de829ad9e765d6b850e1bc34d06fbe1b0e04
sairina <sairina.merinotsui@gmail.com>
2022-02-02 13:36:22 -0800

    Add tests to KDS

---
+++ jest.conf/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const moduleNameMapper = {
+  '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$': path.resolve(
+    __dirname,
+    './fileMock.js'
+  ),
+};
+
+module.exports = {
+  rootDir: path.resolve(__dirname,'..'),
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleNameMapper,
+  testEnvironment: 'jsdom',
+  testURL: 'http://kolibri.time',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  setupFilesAfterEnv: [path.resolve(__dirname, './setup')],
+  verbose: true,
+};
---
+++ jest.conf/setup.js
@@ -0,0 +1,48 @@
+import 'regenerator-runtime/runtime';
+import '@testing-library/jest-dom';
+import * as Aphrodite from 'aphrodite';
+import * as AphroditeNoImportant from 'aphrodite/no-important';
+
+import Vue from 'vue';
+import KThemePlugin from '../lib/KThemePlugin';
+import KContentPlugin from '../lib/content/KContentPlugin';
+
+global.beforeEach(() => {
+  return new Promise(resolve => {
+    Aphrodite.StyleSheetTestUtils.suppressStyleInjection();
+    AphroditeNoImportant.StyleSheetTestUtils.suppressStyleInjection();
+    return process.nextTick(resolve);
+  });
+});
+
+global.afterEach(() => {
+  return new Promise(resolve => {
+    Aphrodite.StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    AphroditeNoImportant.StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+    return process.nextTick(resolve);
+  });
+});
+
+// Register Vue plugins and components
+Vue.use(KThemePlugin);
+Vue.use(KContentPlugin);
+
+Vue.config.silent = true;
+Vue.config.devtools = false;
+Vue.config.productionTip = false;
+
+Object.defineProperty(window, 'scrollTo', { value: () => {}, writable: true });
+
+// Shows better NodeJS unhandled promise rejection errors
+process.on('unhandledRejection', (reason, p) => {
+  /* eslint-disable no-console */
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  console.log(reason.stack);
+});
+
+// Copied from https://github.com/kentor/flush-promises/blob/f33ac564190c784019f1f689dd544187f4b77eb2/index.js
+global.flushPromises = function flushPromises() {
+  return new Promise(function(resolve) {
+    setImmediate(resolve);
+  });
+};
