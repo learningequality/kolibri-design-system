4353c09e6b9f4718b802a434c0531528d9d78c83
MisRob <robosovam@gmail.com>
2022-08-24 12:38:34 +0200

    Add labels example to documentation

--- docs/pages/kcheckbox.vue
+++ docs/pages/kcheckbox.vue
@@ -46,6 +46,54 @@
         A user cannot enter the indeterminate state by interacting directly with the checkbox; it only occurs due to external interactions.
       </p>
     </DocsPageSection>
+
+    <DocsPageSection title="Example: Label content" anchor="#example-labels">
+      Label content can be passed via the <code>label</code> property:
+
+      <!-- eslint-disable -->
+      <!-- prevent prettier from changing indentation -->
+      <DocsShowCode language="html">
+        &lt;KCheckbox label="First lesson" /&gt;
+      </DocsShowCode>
+      <!-- eslint-enable -->
+      <DocsShow>
+        <KCheckbox label="First lesson" />
+      </DocsShow>
+
+      In more complex situations, for example when another component is responsible for rendering the label, the default slot can be used:
+
+      <!-- eslint-disable -->
+      <!-- prevent prettier from changing indentation -->
+      <DocsShowCode language="html">
+        &lt;KCheckbox&gt;
+          &lt;KLabeledIcon icon="lesson" label="First lesson" /&gt;
+        &lt;/KCheckbox&gt;
+      </DocsShowCode>
+      <!-- eslint-enable -->
+      <DocsShow>
+        <KCheckbox>
+          <KLabeledIcon icon="lesson" label="First lesson" />
+        </KCheckbox>
+      </DocsShow>
+
+      <DocsDoNot>
+        <template #not>
+          <p>Don't wrap the default slot's content in <code>&lt;label&gt;</code>:</p>
+          <!-- eslint-disable -->
+          <!-- prevent prettier from changing indentation -->
+          <DocsShowCode language="html">
+            &lt;KCheckbox&gt;
+              &lt;label&gt;
+                &lt;KLabeledIcon icon="lesson" label="First lesson" /&gt;
+              &lt;/label&gt;
+            &lt;/KCheckbox&gt;
+          </DocsShowCode>
+          <!-- eslint-enable -->
+
+          <p>That would cause two nested <code>&lt;label&gt;</code> elements to be rendered as <code>KCheckbox</code> takes care of it already.</p>
+        </template>
+      </DocsDoNot>
+    </DocsPageSection>
   </DocsPageTemplate>
 
 </template>
