92d92a33105bf721cae04cc83b3c8ae969a215f4
Richard Tibbles <richard@learningequality.org>
2022-08-29 16:58:46 -0700

    Update documentation for KSelect and filters.

--- docs/pages/kselect.vue
+++ docs/pages/kselect.vue
@@ -21,21 +21,35 @@
 
     <DocsPageSection title="Usage" anchor="#usage">
       <p>
-        Use a select dropdown for the following:
+        Select dropdowns are used alongside form components such as text fields and radio selects. They can also be used to filter options in lists and tables. 
       </p>
-      <ul>
-        <li></li>
-      </ul>
     </DocsPageSection>
 
     <DocsPageSection title="Specifications" anchor="#specifications">
+      <p>
+        Default width:
+      </p>
       <ul>
+        <li>As a filter for lists and tables: 150px</li>
+        <li>In a form: 300px</li>
       </ul>
+      <p>Increase or decrease this width to fit appropriately with other components in forms or filter groups.</p>
+      <p>The select label should be very short and descriptive. Stick to using one word to describe the field, if possible.</p>
     </DocsPageSection>
 
     <DocsPageSection title="Guidelines" anchor="#guidelines">
-      <ul>
-      </ul>
+      <p>
+        Consider using a select when there are 4 or more options available to select for a field. With fewer options, consider using radios or checkboxes.
+      </p>
+      <h3>Forms</h3>
+      <p>
+        Sometimes the same select field is used in forms for different variations of an editable object (e.g. the “completion” field of a PDF document vs a video file). If a select option for a particular variation of an editable object is not applicable or will create a confusing or negative user experience, it should be hidden from the select. If all options will create the same adverse experience, the entire select should be hidden from that form.
+      </p>
+      <p>
+        Hiding is better than disabling in this case because there is no condition where it would benefit the user experience to be selectable for that object.
+      </p>
+      <h3>Filters</h3>
+      <p>For design guidance on using selects for filtering, see the <DocsInternalLink href="filters" text="Filters page" />.</p>
     </DocsPageSection>
 
   </DocsPageTemplate>
