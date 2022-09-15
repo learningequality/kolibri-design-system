05a3e431ebe817e3bc92c614e5ba3d120fce3a1e
MisRob <robosovam@gmail.com>
2022-09-15 13:08:15 +0200

    Add more information about recent updates to the changelog
    and clean up documentation (keep information about recent
    changes in the changelog rather than in docs + couple minor
    updates)
    
    Follow-up to
    https://github.com/learningequality/kolibri-design-system/pull/346

--- docs/pages/kdropdownmenu.vue
+++ docs/pages/kdropdownmenu.vue
@@ -10,7 +10,6 @@
         Notable possible for configuring the menu include: icons, text, secondary text, and dividers.
       </p>
       <p>
-        Prior to 09/2022, this component also managed the button associated with the dropdown menu, but the API has changed for a clearer division of responsibilities. 
         Please see the <DocsInternalLink href="/buttons#dropdowns" text="Dropdown section of the Buttons and links page" /> on the buttons page for more details about how to use with a button, and a code example.
       </p>
     </DocsPageSection>
