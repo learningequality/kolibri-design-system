ad681ddf560b77b5a2bfa717d01fcf0d47eca207
Samson Akol <akolson2010@gmail.com>
2022-10-05 15:41:46 +0300

    Configures composition api, and documents the useKResponsiveWindow

--- docs/pages/kresponsivewindow.vue
+++ docs/pages/kresponsivewindow.vue
@@ -42,9 +42,41 @@
       <p>
         Provided reactive properties can then be accessed on the component instance via <code>this</code>.
       </p>
+
+      <h3>As composable</h3>
+      <p>It can be imported as <code>useKResponsiveWindow</code>. Register it in the script part of a component file:</p>
+
+      <!-- eslint-disable -->
+      <!-- prevent prettier from changing indentation -->
+      <DocsShowCode language="javascript">
+        import useKResponsiveWindow from 'kolibri-design-system/lib/useKResponsiveWindow';
+
+        export default {
+          setup() {
+            const {
+              windowWidth,
+              windowIsLarge,
+              windowBreakpoint,
+              ...
+            } = useKResponsiveWindow();
+
+            //Peform some operations with the properties
+
+            return {
+              windowWidth,
+              windowIsLarge,
+              windowBreakpoint,
+              ...
+            };
+          }
+        };
+      </DocsShowCode>
+      <!-- eslint-enable -->
+
+      <p><code>useKResponsiveWindow()</code> returns an object of reactive properties, that you could destruture.</p>
     </DocsPageSection>
 
-    <DocsPageSection title="Example" anchor="#example">
+    <DocsPageSection title="Mixin Example" anchor="#mixin-example">
       <p>
         Consider a Vue file with this in its template and script:
       </p>
@@ -87,6 +119,55 @@
       <p>
         Try adjusting your browser window size to see the example in action.
       </p>
+    </DocsPageSection>ßßßß
+
+    <DocsPageSection title="Composable Example" anchor="#composable-example">
+      <p>
+        Consider a Vue file with this in its template and script:
+      </p>
+      <!-- eslint-disable -->
+        <!-- prevent prettier from changing indentation -->
+        <DocsShowCode language="html">
+          <div class="box" :style="boxStyle">
+            Box 1
+          </div>
+          <div class="box" :style="boxStyle">
+            Box 2
+          </div>
+        </DocsShowCode>
+        <DocsShowCode language="javascript">
+          setup() {
+            ...
+
+            const boxStyle = computed(() => {
+              return { display: windowIsLarge.value ? 'inline-block' : 'block' };
+            });
+
+            return { 
+              ..., 
+              boxStyle,
+            };
+          }
+        </DocsShowCode>
+        <!-- eslint-enable -->
+      <p>
+        This results in two boxes that stack vertically on small screens and otherwise display side-by-side:
+      </p>
+      <DocsShow>
+        <div>Breakpoint level: {{ _windowBreakpoint }}</div>
+        <div>Window is large: {{ _windowIsLarge }}</div>
+        <div>
+          <div class="box" :style="_boxStyle">
+            Box 1
+          </div>
+          <div class="box" :style="_boxStyle">
+            Box 2
+          </div>
+        </div>
+      </DocsShow>
+      <p>
+        Try adjusting your browser window size to see the example in action.
+      </p>
     </DocsPageSection>
 
     <DocsPageSection title="Related" anchor="#related">
@@ -106,10 +187,24 @@
 
 <script>
 
+  import { computed } from '@vue/composition-api';
   import responsiveWindowMixin from '~~/lib/KResponsiveWindowMixin.js';
+  import useKResponsiveWindow from '~~/lib/useKResponsiveWindow.js';
 
   export default {
     mixins: [responsiveWindowMixin],
+    setup() {
+      const {
+        windowBreakpoint: _windowBreakpoint,
+        windowIsLarge: _windowIsLarge,
+      } = useKResponsiveWindow();
+
+      const _boxStyle = computed(() => {
+        return { display: _windowIsLarge.value ? 'inline-block' : 'block' };
+      });
+
+      return { _windowBreakpoint, _windowIsLarge, _boxStyle };
+    },
     computed: {
       boxStyle() {
         return { display: this.windowIsLarge ? 'inline-block' : 'block' };
