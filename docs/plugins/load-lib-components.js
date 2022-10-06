76c13a8df572a4c9dee2f6102f257f5d5a5b3c34
Samson Akol <akolson2010@gmail.com>
2022-10-06 15:12:43 +0300

    Moves composition api initialization to useKResponsiveWindow

--- docs/plugins/load-lib-components.js
+++ docs/plugins/load-lib-components.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueCompositionAPI from '@vue/composition-api';
 import KThemePlugin from '~~/lib/KThemePlugin';
 import trackInputModality from '~~/lib/styles/trackInputModality';
 
@@ -8,5 +7,4 @@ import trackInputModality from '~~/lib/styles/trackInputModality';
 // See `KThemePlugin` `$coreOutline` and `globalThemeState.inputModality`
 trackInputModality({ disableFocusRingByDefault: false });
 
-Vue.use(VueCompositionAPI);
 Vue.use(KThemePlugin);
