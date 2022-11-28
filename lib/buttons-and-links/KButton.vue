ac7d7bfa77754b70b6376c8913999072bd764379
Marcella Maki <marcellamaki@gmail.com>
2022-11-28 18:24:36 -0500

    Just, redo that whole approach. Refactor and un-update docs

--- lib/buttons-and-links/KButton.vue
+++ lib/buttons-and-links/KButton.vue
@@ -214,10 +214,4 @@
     top: 4px;
   }
 
-  // adds this class to mock disabled item behavior, which can be used for a tags
-  .disabled {
-    pointer-events: none;
-    cursor: default;
-  }
-
 </style>
