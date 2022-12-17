b80b677482c8fc0d6d0bdceb977fe270175128e6
Alex Velez <alanvele@espol.edu.ec>
2022-12-17 12:53:29 -0500

    [KOptionalText] New component

---
+++ lib/KOptionalText.vue
@@ -0,0 +1,79 @@
+<template>
+
+  <span v-if="!textEmpty" :style="textStyle">
+    <template v-if="text">
+      {{ text }}
+    </template>
+    <template v-else>
+      <slot></slot>
+    </template>
+  </span>
+
+  <KEmptyPlaceholder v-else />
+
+</template>
+
+
+<script>
+
+  import KEmptyPlaceholder from './KEmptyPlaceholder.vue';
+
+  export default {
+    name: 'KOptionalText',
+    components: {
+      KEmptyPlaceholder,
+    },
+    props: {
+      /**
+       * Text to display
+       */
+      text: {
+        type: String,
+        default: null,
+      },
+      /**
+       * If provided, sets the styles of the text
+       */
+      textStyle: {
+        type: Object,
+        default: null,
+      },
+    },
+    computed: {
+      textEmpty() {
+        if (this.text) {
+          return false;
+        }
+
+        const slotsText = this.getSlotsText();
+        return !slotsText.trim();
+      },
+    },
+    methods: {
+      /**
+       * Returns the text to display
+       */
+      getSlotsText() {
+        if (!this.$slots.default || !this.$slots.default.length) {
+          return '';
+        }
+
+        return this.$slots.default
+          .map(node => {
+            if (node.text) {
+              return node.text;
+            }
+            if (node.children) {
+              return node.children.map(child => child.text).join('');
+            }
+            return '';
+          })
+          .join('');
+      },
+    },
+  };
+
+</script>
+
+
+<style lang="scss" scoped></style>
