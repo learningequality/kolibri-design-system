ae3438e36b26c802bd4815caf482a0399963fdd7
thanksameeelian <ameliabreault@gmail.com>
2023-02-08 11:44:56 -0600

    temporary files for ease of testing kselect within a kmodal

--- docs/tableOfContents.js
+++ docs/tableOfContents.js
@@ -260,6 +260,11 @@ export default [
         keywords: ['field', 'box'],
       }),
       new Page({
+        path: '/kselectinkmodal',
+        title: 'KSelectInKModal',
+        isCode: true,
+      }),
+      new Page({
         path: '/kswitch',
         title: 'KSwitch',
         isCode: true,
