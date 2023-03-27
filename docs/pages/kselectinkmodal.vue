ddd464aea5029661f5a89392c186eea7dca59c15
thanksameeelian <ameliabreault@gmail.com>
2023-03-27 17:39:47 -0500

    set up for kselect in kmodal cohacking

---
+++ docs/pages/kselectinkmodal.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <DocsPageTemplate>
+    <DocsPageSection title="Overview" anchor="#overview">
+      <DocsShow>
+        <KModal
+          :title="'KSelect inside KModal'"
+        >
+          <KSelect
+            v-model="exampleData"
+            :options="options"
+            label="Who are you?"
+            placeholder="Nobody"
+          />
+          <p>Value: {{ exampleData }}</p>
+        </KModal>
+      </DocsShow>
+    </DocsPageSection>
+  </DocsPageTemplate>
+
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        exampleData: null,
+        options: [
+          {
+            label: 'Somebody',
+            value: 'somebody',
+          },
+          {
+            label: 'You used to know me',
+            value: 'usedto',
+          },
+          {
+            label: 'friend',
+            value: 'friend',
+          },
+          {
+            label: 'roman',
+            value: 'roman',
+          },
+          {
+            label: 'countryman',
+            value: 'countryman',
+          },
+          {
+            label: 'abraham lincoln',
+            value: 'lincoln',
+          },
+          {
+            label: 'albert einstein',
+            value: 'einstein',
+          },
+          {
+            label: 'bob barker',
+            value: 'barker',
+          },
+          {
+            label: 'billie jean',
+            value: 'billiejean',
+          },
+        ],
+      };
+    },
+  };
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ lib/KSelectinKModal.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'KSelectInKModal',
+  };
+
+</script>
+
+<style scoped>
+
+</style>
