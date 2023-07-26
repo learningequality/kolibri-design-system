f9fcaf0f98044dcb3157f25eb3716cf0eb3169ba
MisRob <robosovam@gmail.com>
2023-07-26 16:41:47 +0200

    Add scaling and ratio logic

--- docs/pages/kimg.vue
+++ docs/pages/kimg.vue
@@ -2,117 +2,204 @@
 
   <DocsPageTemplate apiDocs>
     <DocsPageSection title="Overview" anchor="#overview">
-      <div>
-        The <DocsLibraryLink component="KImg" /> component displays images based on an image source and optional image
-        dimensions provided by the implementer.
-
-        <h3>Sample implementations of <DocsLibraryLink component="KImg" /> including props:</h3>
-
-        <div>
-          Dimensions may be either numbers or strings consisting of a numeral and valid units (e.g. <code>px</code>,
-          <code>em</code>, <code>vh</code>).
-        </div>
-
-        <div class="img-example-1">
-          <DocsShow>
-            <KImg
-              :src="require('../assets/img_sample_for_kimg.png')"
-              altText="Computers are run by bees"
-              :height="'250.2px'"
-              :width="225.5"
-              :maxHeight="600"
-              :minWidth="25"
-            />
-          </DocsShow>
-          <DocsShowCode language="html">
-            <KImg
-              :src="require('../assets/img_sample_for_kimg.png')"
-              altText="Computers are run by bees"
-              :height="'250.2px'"
-              :width="225.5"
-            />
-          </DocsShowCode>
-        </div>
-      </div>
-
-      <div class="img-example-2">
-        <DocsLibraryLink component="KImg" /> requires alternative text that describes the image unless
-        <code>isDecorative</code> is <code>true</code>. In that case, any alt text provided will be overwritten to an
-        empty string.
-        <div>
-          <div>
-            <DocsShow>
-              <KImg
-                :src="require('../assets/img_sample_for_kimg.png')"
-                isDecorative
-                :height="50"
-                :width="50"
-              />
-            </DocsShow>
-          </div>
-          <DocsShowCode language="html">
-            <KImg
-              :src="require('../assets/img_sample_for_kimg.png')"
-              isDecorative
-              :height="50"
-              :width="50"
-            />
-          </DocsShowCode>
-        </div>
-      </div>
-
-      <div>
-        If dimensions for the image are not specified, the size will default to the height and width of the source
-        image.
-        <DocsShow>
-          <KImg
-            :src="require('../assets/img_sample_for_kimg.png')"
-            altText="Computers are run by bees"
-          />
-        </DocsShow>
-        <DocsShowCode language="html">
-          <KImg
-            :src="require('../assets/img_sample_for_kimg.png')"
-            altText="Computers are run by bees"
-          />
-        </DocsShowCode>
-      </div>
-
-      <div>
-        You can also use the <DocsInternalLink href="#prop:appearanceOverrides">
-          appearanceOverrides prop
-        </DocsInternalLink> to add custom styles to the image. It accepts any Vue dynamic styles object and applies the same with the highest precedence.
-        <DocsShow>
-          <KImg
-            :src="require('../assets/img_sample_for_kimg.png')"
-            altText="Computers are run by bees"
-            :width="'100%'"
-            :appearanceOverrides="{
-              width: '50%',
-              border: '2px solid black',
-              padding: '10px',
-              borderRadius: '5px'
-            }
-            "
-          />
-        </DocsShow>
-        <DocsShowCode language="html">
-          <KImg
-            :src="require('../assets/img_sample_for_kimg.png')"
-            altText="Computers are run by bees"
-            :width="'100%'"
-            :appearanceOverrides="{
-              width: '50%',
-              border: '2px solid black',
-              padding: '10px',
-              borderRadius: '5px'
-            }
-            "
-          />
-        </DocsShowCode>
-        Even thought the <code>width</code> prop specifies the width of the image to be <code>100%</code>, the <code>appearanceOverrides</code> prop overrides it and sets the width to <code>50%</code>.
-      </div>
+      <p>It displays an image and provides additional functionality to manipulate it such as setting dimensions, aspect ratio, scaling, letter boxing, and more.</p>
     </DocsPageSection>
+
+    <DocsPageSection title="Usage" anchor="#usage">
+      <p>When setting the dimensions of an image by using any of the supported methods, note that the settings are applied to the container of the image, rather than the image itself. Depending on the scale type and other settings, the image may be letterboxed. If you need to apply dimensions directly to the image, you can use <code>'fitXY'</code> scale type. This, and other modes, are illustrated in the examples below, where the original image dimensions are 200×114 px.</p>
+
+      <p>Unless you set fixed dimensions, <code>KImg</code> is responsive by default.</p>
+
+      <h3>Rendering within inline and block elements</h3>
+
+      <h4>Inline</h4>
+
+      <p>When rendered within an inline element, an image preserves its original dimensions.</p>
+
+      <DocsShowCode language="html">
+        <span>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+          />
+        </span>
+      </DocsShowCode>
+      <DocsShow>
+        <span>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+          />
+        </span>
+      </DocsShow>
+
+
+      <h4>Block</h4>
+
+      <p>When rendered within a block element, an image scales to its parent element with the <code>'centerInside'</code> scale type (see below) by default.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+          />
+        </div>
+      </DocsShowCode>
+      <DocsShow block>
+        <div>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+          />
+        </div>
+      </DocsShow>
+
+      <h3>Dimensions</h3>
+
+      <p>You can use the most common dimensions right on the <code>KImg</code> via props like <code>width</code>, <code>maxHeight</code>, etc. See <a href="#props">Props</a> for the full list of available props. Values may be either numbers or strings consisting of a numeral and valid units. The following units are supported: <code>%, cm, em, ex, ch, in, lh, mm, px, rem, rlh, vh, vw</code>. If you don't provide a unit, <code>px</code> will be used by default.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+            height="250.2px"
+            width="100%"
+            maxWidth="10vw"
+            :minWidth="25"
+          />
+        </div>
+      </DocsShowCode>
+
+      <h3>Alternative text</h3>
+
+      <p>Unless an image is <DocsExternalLink text="decorative" href="https://www.w3.org/WAI/tutorials/images/decorative/" />, you need to provide alternative text via <code>altText</code>. If it's meant to be decorative, then use <code>isDecorative</code> to indicate it. Then, alternative text won't be required, and the image will be hidden from assistive technologies.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            isDecorative
+          />
+        </div>
+      </DocsShowCode>
+
+      <h3>Scaling</h3>
+
+      <h4><code style="font-weight: bold">'centerInside'</code> scale type</h4>
+
+      <p>Scales an image uniformly and maintains its original aspect ratio so that both its width and height are equal to or less than the container. The image can be letterboxed. It's the safest mode as it never distorts the image.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="centerInside"
+          />
+        </div>
+      </DocsShowCode>
+      <DocsShow block>
+        <div>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="centerInside"
+          />
+        </div>
+      </DocsShow>
+
+      <h4><code style="font-weight: bold">'contain'</code> scale type</h4>
+
+      <p>Behaves like <code>'centerInside'</code> except it ensures that at least one axis of the image fits the container exactly. The original aspect ratio is maintained. The image can be letterboxed. This mode may distort the image by enlarging it above its original size.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="contain"
+          />
+        </div>
+      </DocsShowCode>
+      <DocsShow block>
+        <div>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="contain"
+          />
+        </div>
+      </DocsShow>
+
+      <h4><code style="font-weight: bold">'fitXY'</code> scale type</h4>
+
+      <p>Scales X and Y axis of an image independently, so that the image matches the container exactly. This mode may distort the image as its original aspect ratio is ignored. The image is never letterboxed. It could also distort the image by enlarging it above its original size.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="fitXY"
+          />
+        </div>
+      </DocsShowCode>
+      <DocsShow block>
+        <div>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+            height="200px"
+            width="100%"
+            maxWidth="500px"
+            scaleType="fitXY"
+          />
+        </div>
+      </DocsShow>
+
+      <h3>Aspect ratio</h3>
+
+      <p>You can set aspect ratio via <code>aspectRatio</code> on an image and combine it with any of the scale types.</p>
+
+      <p>Note that ratio styles are based on the width information, therefore it needs to be available in some way. For example, it could be provided directly to <code>KImg</code>, or you could ensure that its parent element has width by setting it explicitly or by using a block element.</p>
+
+      <DocsShowCode language="html">
+        <div>
+          <KImg
+            src="hummingbird.jpg"
+            altText="A sitting hummingbird"
+            aspectRatio="4:3"
+          />
+        </div>
+      </DocsShowCode>
+      <DocsShow block>
+        <div>
+          <KImg
+            :src="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+            altText="A sitting hummingbird"
+            aspectRatio="4:3"
+          />
+        </div>
+      </DocsShow>
+    </DocsPageSection>
+
   </DocsPageTemplate>
 
 </template>
@@ -125,29 +212,4 @@
 </script>
 
 
-<style lang="scss" scoped>
-
-  .img-example-1 {
-    display: flex;
-    margin-top: 20px;
-  }
-
-  .img-example-2 {
-    margin-top: 20px;
-    margin-bottom: 20px;
-  }
-
-  .img-example-2 > div {
-    display: flex;
-  }
-
-  .img-example-2 > div > div {
-    margin-top: 30px;
-  }
-
-</style>
-
-
-
-
-
+<style lang="scss" scoped></style>
--- lib/KImg/index.vue
+++ lib/KImg/index.vue
@@ -1,20 +1,58 @@
 <template>
 
-  <div :style="containerStyles">
-    <img
-      :src="src"
-      :alt="alternateText"
-      :style="imgStyles"
-      @error="onError"
-    >
-    <slot></slot>
-  </div>
+  <span :style="rootContainerStyles">
+    <span :style="ratioContainerStyles">
+      <img
+        :src="src"
+        :alt="alternativeText"
+        :style="imgStyles"
+        @error="onError"
+      >
+      <!-- TODO: Document slot -->
+      <slot></slot>
+    </span>
+  </span>
 
 </template>
 
 
 <script>
 
+  const ScaleTypes = {
+    CENTER_INSIDE: 'centerInside',
+    CONTAIN: 'contain',
+    FIT_XY: 'fitXY',
+  };
+  const RATIO_REGEX = /^(\d+):(\d+)$/;
+
+  function isValidScaleType(value) {
+    return value && Object.values(ScaleTypes).includes(value);
+  }
+
+  /**
+   * Extract x and y from an aspect ratio x:y
+   *
+   * @param {String} value aspect ratio in "x:y" format
+   * @returns {Object} { x, y }
+   */
+  function extractAspectRatio(value) {
+    const match = value.match(RATIO_REGEX);
+    return { x: match[1], y: match[2] };
+  }
+
+  /**
+   * @returns {Boolean} Returns true if value satisfies "x:y"
+   *                    format where x and y are numbers
+   */
+  function isValidAspectRatio(value) {
+    return RATIO_REGEX.test(value);
+  }
+
+  /**
+   * Displays an image and provides additional functionality
+   * to manipulate it such as setting an aspect ratio,
+   * scaling, and more.
+   */
   export default {
     name: 'KImg',
     props: {
@@ -26,60 +64,79 @@
         default: null,
       },
       /**
-       * Alternate text for the image. This is required and will throw a warning when it's not provided (empty) unless isDecorative is true
+       * Alternative text for the image. Required unless `isDecorative` is true.
        */
       altText: {
         type: String,
         default: '',
       },
       /**
-       * Sets the image as decorative. This sets the alt image property to an empty string.
+       * Sets the image as decorative.
        */
       isDecorative: {
         type: Boolean,
         default: false,
       },
       /**
-       * Sets the height for the container
+       * Sets the height of the image container
        */
       height: {
         type: [Number, String],
         default: undefined,
       },
       /**
-       * Sets the width for the container
+       * Sets the width of the image container
        */
       width: {
         type: [Number, String],
         default: undefined,
       },
       /**
-       * Sets the maximum height for the container
+       * Sets the maximum height of the image container
        */
       maxHeight: {
         type: [Number, String],
         default: undefined,
       },
       /**
-       * Sets the minimum height for the container
+       * Sets the minimum height of the image container
        */
       minHeight: {
         type: [Number, String],
         default: undefined,
       },
       /**
-       * Sets the maximum width for the container
+       * Sets the maximum width of the image container
        */
       maxWidth: {
         type: [Number, String],
         default: undefined,
       },
       /**
-       * 	Sets the minimum width for the container
+       * 	Sets the minimum width of the image container
        */
       minWidth: {
         type: [Number, String],
         default: undefined,
+      },
+      /**
+       * Sets the ratio of the width(w) to the height(h)
+       * of the image container. The required format is `w:h`.
+       */
+      aspectRatio: {
+        type: String,
+        default: undefined,
+        validator: isValidAspectRatio,
+      },
+      /**
+       * Specifies how an image should be scaled within the container.
+       * Can be one of  `'centerInside'` (default), `'contain'`, or `'fitXY'`.
+       * See the documentation examples.
+       */
+      scaleType: {
+        type: String,
+        default: 'centerInside', // needs to be duplicated rather than using ScaleTypes.CENTER_INSIDE, otherwise it doesn't render correctly in the auto-generated Props documentation
+        validator: isValidScaleType,
       },
       /**
        * Accepts a Vue dynamic styles object to override the default styles to modify the appearance of the component.
@@ -91,24 +148,128 @@
       },
     },
     computed: {
-      alternateText() {
+      ratio() {
+        return extractAspectRatio(this.aspectRatio);
+      },
+      alternativeText() {
         return this.isDecorative ? '' : this.altText;
       },
-      containerStyles() {
+      baseStyles() {
         return {
-          height: this.validateAndFormatUnits(this.height),
-          width: this.validateAndFormatUnits(this.width),
-          maxHeight: this.validateAndFormatUnits(this.maxHeight),
-          minHeight: this.validateAndFormatUnits(this.minHeight),
-          maxWidth: this.validateAndFormatUnits(this.maxWidth),
-          minWidth: this.validateAndFormatUnits(this.minWidth),
+          rootContainer: {
+            display: 'block',
+            backgroundColor: this.$themePalette.grey.v_200,
+            height: this.validateAndFormatUnits(this.height),
+            width: this.validateAndFormatUnits(this.width),
+            maxHeight: this.validateAndFormatUnits(this.maxHeight),
+            minHeight: this.validateAndFormatUnits(this.minHeight),
+            maxWidth: this.validateAndFormatUnits(this.maxWidth),
+            minWidth: this.validateAndFormatUnits(this.minWidth),
+          },
+          ratioContainer: {
+            display: 'block',
+          },
+          img: {
+            display: 'block',
+          },
+        };
+      },
+      scaleStyles() {
+        const scaleKind = isValidScaleType(this.scaleType) ? this.scaleType : ScaleTypes.CONTAIN;
+        const scaleStyles = {
+          [ScaleTypes.CONTAIN]: {
+            rootContainer: {},
+            ratioContainer: {
+              height: '100%',
+              width: '100%',
+            },
+            img: {
+              objectFit: 'contain',
+              height: '100%',
+              width: '100%',
+            },
+          },
+          [ScaleTypes.FIT_XY]: {
+            rootContainer: {},
+            ratioContainer: {
+              height: '100%',
+              width: '100%',
+            },
+            img: {
+              width: '100%',
+              height: '100%',
+            },
+          },
+          [ScaleTypes.CENTER_INSIDE]: {
+            rootContainer: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+            ratioContainer: {},
+            img: {
+              maxWidth: '100%',
+              maxHeight: '100%',
+            },
+          },
+        };
+        return scaleStyles[scaleKind];
+      },
+      ratioStyles() {
+        if (!this.aspectRatio) {
+          return {
+            rootContainer: {},
+            ratioContainer: {},
+            img: {},
+          };
+        }
+        const paddingTopInPercent = (this.ratio.y / this.ratio.x) * 100;
+        return {
+          rootContainer: {},
+          ratioContainer: {
+            position: 'relative',
+            display: 'block',
+            height: 0,
+            width: '100%',
+            padding: `${paddingTopInPercent}% 0 0`,
+          },
+          img: {
+            position: 'absolute',
+            display: 'block',
+            maxWidth: '100%',
+            maxHeight: '100%',
+            margin: 'auto',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 2,
+          },
+        };
+      },
+      rootContainerStyles() {
+        // order matters
+        return {
+          ...this.baseStyles.rootContainer,
+          ...this.scaleStyles.rootContainer,
+          ...this.ratioStyles.rootContainer,
           ...this.appearanceOverrides,
         };
       },
-      imgStyles() {
+      ratioContainerStyles() {
+        // order matters
         return {
-          width: '100%',
-          height: '100%',
+          ...this.baseStyles.ratioContainer,
+          ...this.scaleStyles.ratioContainer,
+          ...this.ratioStyles.ratioContainer,
+        };
+      },
+      imgStyles() {
+        // order matters
+        return {
+          ...this.baseStyles.img,
+          ...this.scaleStyles.img,
+          ...this.ratioStyles.img,
         };
       },
     },
