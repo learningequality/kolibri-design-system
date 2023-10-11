abb8af1f48d74fe7a070a9733167281b00fb6e7d
Jacob Pierce <w.jacobpierce@gmail.com>
2023-10-10 21:26:07 -0700

    KDropdownMenu drill `containFocus` prop into UiPopover
    Rather than defaulting to true, give the user the option here

--- lib/KDropdownMenu.vue
+++ lib/KDropdownMenu.vue
@@ -3,7 +3,7 @@
   <UiPopover
     ref="popover"
     :z-index="12"
-    :containFocus="true"
+    :containFocus="containFocus"
     :dropdownPosition="position"
     @close="handleClose"
     @open="handleOpen"
@@ -51,6 +51,13 @@
       hasIcons: {
         type: Boolean,
         default: false,
+      },
+      /**
+       *
+       */
+      containFocus: {
+        type: Boolean,
+        default: true,
       },
       /**
        * The position of the dropdown relative to the button
