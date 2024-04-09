d82513c6ad9de6420add5770d6035f20ad52ef40
satyam kale <94701487+satyamkale27@users.noreply.github.com>
2024-04-09 08:41:10 +0000

    made KTooltip's width configurable

--- lib/KTooltip/index.vue
+++ lib/KTooltip/index.vue
@@ -11,7 +11,7 @@
     <div
       dir="auto"
       class="k-tooltip"
-      :style="{ backgroundColor: $themeTokens.text, color: $themeTokens.textInverted }"
+      :style="{ backgroundColor: $themeTokens.text, color: $themeTokens.textInverted, maxWidth: maxWidth ? maxWidth : 'calc(100vw - 10px)', }"
     >
       <!-- If text prop is provided, display the text -->
       <template v-if="text"> 
@@ -75,6 +75,10 @@
         type: String,
         default: null,
       },
+      maxWidth: {
+        type: String,
+        default: null,
+      },
     },
     data() {
       return {
@@ -125,7 +129,6 @@
     z-index: 24;
     width: max-content;
     min-width: 75px;
-    max-width: calc(100vw - 10px);
     padding: 8px;
     font-size: 12px;
     font-weight: normal;
