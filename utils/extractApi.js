81d1c5ba8d79ac24aec598c9d19cf8b5765b4532
Blaine Jester <blainesworld@gmail.com>
2024-05-23 09:34:31 -0700

    Update imports to use node prefix

--- utils/extractApi.js
+++ utils/extractApi.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('node:path');
+const fs = require('node:fs');
 const docGenAPI = require('vue-docgen-api');
 const globby = require('globby');
 const consola = require('consola');
