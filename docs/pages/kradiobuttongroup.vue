a8c195153fb6f848359043868550a02e2b3f20e0
Mudit Choudhary <cmudit60@gmail.com>
2024-06-04 16:10:42 +0530

    Add docs for KRadioButtonGroup
    - Add KRadioButton method description
    - Add KRadioButtonGroup slot and props description

---
+++ docs/pages/kradiobuttongroup.vue
@@ -0,0 +1,55 @@
+
+<template>
+
+  <DocsPageTemplate :apiDocs="true">
+
+    <DocsPageSection title="Overview" anchor="#overview">
+      <p>
+        Allows movement between radio buttons inside a radio group using arrow keys in FireFox.
+      </p>
+      <DocsShow>
+        <KRadioButtonGroup>
+          <KRadioButton
+            v-model="exampleValue"
+            label="Option A"
+            value="val-a"
+          />
+          <KRadioButton
+            v-model="exampleValue"
+            label="Option B"
+            value="val-b"
+          />
+          <KRadioButton
+            v-model="exampleValue"
+            label="Option C"
+            description="This one is special!"
+            buttonValue="val-c"
+          />
+          <KRadioButton
+            v-model="exampleValue"
+            label="Truncated label. Adjusting your browser window size to see this in action."
+            buttonValue="val-d"
+            truncateLabel
+          />
+        </KRadioButtonGroup>
+        <p>
+          Current value: {{ exampleValue }}
+        </p>
+      </DocsShow>
+    </DocsPageSection>
+
+  </DocsPageTemplate>
+
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        exampleValue: 'val-b',
+      };
+    },
+  };
+
+</script>
