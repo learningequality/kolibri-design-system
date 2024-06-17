98a819daa7479d321b25a2e0341df671d50805e7
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2024-06-17 23:36:49 +0300

    redesigned card content

--- docs/pages/playground.vue
+++ docs/pages/playground.vue
@@ -25,94 +25,52 @@
     <!-- Play around with your component here: -->
     <h4>Vertical layout</h4>
 
-    <ul style="width:30%;">
+    <ul style="width:35%;">
 
       <KCard
         :to="{ name: '/' }"
         :headingLevel="2"
         layout="horizontal"
         thumbnailDisplay="none"
-        :thumbnailSrc="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
       >
         <template #title>
           <div>
             <KTextTruncator
-              text="The Philosophy of Rick and Morty and the meaning of All life"
+              text="This is a sample lesson title that goes to two lines and truncates at the end of the two lines This is a sample title that goes"
               :maxLines="2"
             />
           </div>
         </template>
         <template #aboveTitle>
-          <div>
-            Rutrum pellentesque utrum...
+          <div :style="{ 'color': $themePalette.grey.v_400,'font-size': '1.2em' }">
+            Class name 1
           </div>
         </template>
         <template #footer>
-          <div>
-            <div class="footer-top-header">
-              <span> <KIcon icon="quiz" /> Practice</span>
-              <span>Short Activity</span>
-            </div>
-            <br>
-            <div class="footer-top-header">
-              <span>
-                <progress class="" value="50" max="100"></progress>
-              </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
-              </span>
-            </div>
-          </div>
+          <KGrid>
+            <KGridItem
+              :layout8="{ span: 4 }"
+              :layout12="{ span: 6 }"
+            >
+              <KIcon
+                icon="mastered"
+                style="font-size:20px;margin-bottom:-5px"
+              />
+              <span>score 30%</span>
+            </KGridItem>
+            <KGridItem
+              :layout8="{ span: 4 }"
+              :layout12="{ span: 6 }"
+            >
+              <div class="" style="float:right">
+                <KIcon icon="optionsVertical" />
+              </div>
+            </KGridItem>
+          </KGrid>
         </template>
       </KCard>
       <br>
 
-      <KCard
-        :to="{ name: '/' }"
-        :headingLevel="2"
-        layout="vertical"
-        thumbnailDisplay="small"
-        :thumbnailSrc="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
-      >
-        <template #title>
-          <div>
-            <KTextTruncator
-              text="The Philosophy of Rick and Morty and the meaning of All life"
-              :maxLines="2"
-            />
-          </div>
-        </template>
-        <template #aboveTitle>
-          <div>
-            Rutrum pellentesque utrum...
-          </div>
-        </template>
-        <template #footer>
-          <div>
-            <div class="footer-top-header">
-              <span> <KIcon icon="quiz" /> Practice</span>
-              <span>Short Activity</span>
-            </div>
-            <br>
-            <div class="footer-top-header">
-              <span>
-                <progress class="" value="50" max="100"></progress>
-              </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
-              </span>
-            </div>
-          </div>
-        </template>
-      </KCard>
-
-      <br>
       <KCard
         :to="{ name: '/' }"
         :headingLevel="2"
@@ -123,124 +81,146 @@
         <template #title>
           <div>
             <KTextTruncator
-              text="This is a sample resource title that goes to three lines and truncates at the end of the three lines This is a sample title that goes to t..."
+              text="This is a sample resource title that goes to three lines and truncates at the end of the three lines This is a sample title that goes"
+              :maxLines="2"
+            />
+          </div>
+        </template>
+        <template #aboveTitle>
+          <KGrid>
+
+            <KGridItem
+              :layout12="{ span: 8 }"
+              :layout8="{ span: 6 }"
+            >
+              <span><KIcon icon="readSolid" class="icon-size" /></span>
+              <span
+                :style="{ 'color': $themePalette.grey.v_400,
+                          'font-size': '1.2em' }"
+              >Rutrum pellentesque utrum...</span>
+            </KGridItem>
+
+            <KGridItem
+              :layout12="{ span: 4 }"
+              :layout8="{ span: 2 }"
+            >
+              <div style="float:right">
+                <KIcon icon="infoOutline" class="icon-size" />
+              </div>
+            </KGridItem>
+          </KGrid>
+        </template>
+        <template #footer>
+          <div>
+            <div class="footer-top-header">
+              <span>
+                <KIcon icon="createShaded" />
+                Practice
+              </span>
+              <span
+                :style="{ 'background-color': $themePalette.grey.v_100 }"
+              >
+                Short Activity
+              </span>
+            </div>
+
+            <KGrid>
+              <KGridItem
+                :layout12="{ span: 8 }"
+                :layout8="{ span: 6 }"
+              >
+                <progress
+                  value="20"
+                  max="100"
+                  style="height:0.5em;width:100%"
+                ></progress>
+              </KGridItem>
+              <KGridItem
+                :layout12="{ span: 4 }"
+                :layout8="{ span: 2 }"
+              >
+                <div class="" style="float:right;">
+                  <KIcon icon="schedule" class="icon-size" />
+                  <KIcon icon="download" class="icon-size" />
+                </div>
+              </KGridItem>
+            </KGrid>
+          </div>
+        </template>
+      </kcard>
+
+      <br>
+
+      <KCard
+        :to="{ name: '/' }"
+        :headingLevel="2"
+        layout="horizontal"
+        thumbnailDisplay="small"
+        :thumbnailSrc="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
+      >
+        <template #title>
+          <div>
+            <KTextTruncator
+              text="This is a sample resource title that goes to three lines and truncates at the end of the three lines This is a sample title that goes"
               :maxLines="3"
             />
           </div>
         </template>
         <template #aboveTitle>
-          <div>
-            <span> <KIcon icon="coachContent" /> </span>
-            Rutrum pellentesque utrum...
-          </div>
+          <KGrid>
+
+            <KGridItem
+              :layout12="{ span: 12 }"
+              :layout8="{ span: 6 }"
+            >
+              <span
+                :style="{ 'color': $themePalette.grey.v_400,
+                          'font-size': '1.2em' }"
+              >Rutrum pellentesque utrum...</span>
+            </KGridItem>
+          </KGrid>
         </template>
         <template #footer>
           <div>
-            <span> <KIcon icon="createSolid" /> </span>
-            <span>Short Activity</span>
-
-            <br>
             <div class="footer-top-header">
               <span>
-                <progress class="" value="50" max="100"></progress>
+                <KIcon icon="practiceSolid" />
+                Practice
               </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
+              <span
+                :style="{ 'background-color': $themePalette.grey.v_100 }"
+              >
+                Short Activity
               </span>
             </div>
+
+            <KGrid>
+              <KGridItem
+                :layout12="{ span: 8 }"
+                :layout8="{ span: 6 }"
+              >
+                <!-- <KIcon icon="schedule" class="icon-size" /> -->
+                <progress
+                  value="20"
+                  max="100"
+                  style="height:0.5em;width:100%"
+                ></progress>
+              </KGridItem>
+              <KGridItem
+                :layout12="{ span: 4 }"
+                :layout8="{ span: 2 }"
+              >
+                <div class="" style="float:right;">
+                  <KIcon icon="download" class="icon-size" />
+                </div>
+              </KGridItem>
+            </KGrid>
           </div>
         </template>
-      </KCard>
-
+      </kcard>
     </ul>
 
-    <h4>Horizontal layout</h4>
-    <ul style="width:30%;">
-      <br>
-      <KCard
-        :to="{ name: '/' }"
-        :headingLevel="2"
-        layout="horizontal"
-        thumbnailDisplay="none"
-        :thumbnailSrc="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
-      >
-        <template #title>
-          <div>
-            <KTextTruncator
-              text="The Philosophy of Rick and Morty and the meaning of All life"
-              :maxLines="2"
-            />
-          </div>
-        </template>
-        <template #aboveTitle>
-          <div>
-            Rutrum pellentesque utrum...
-          </div>
-        </template>
-        <template #footer>
-          <div>
-            <div class="footer-top-header">
-              <span> <KIcon icon="quiz" /> Practice</span>
-              <span>Short Activity</span>
-            </div>
-            <br>
-            <div class="footer-top-header">
-              <span>
-                <progress class="" value="50" max="100"></progress>
-              </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
-              </span>
-            </div>
-          </div>
-        </template>
-      </KCard>
-
-      <br>
-      <KCard
-        :to="{ name: '/' }"
-        :headingLevel="2"
-        layout="horizontal"
-        thumbnailDisplay="small"
-        :thumbnailSrc="require('../assets/hummingbird CC BY-SA 4.0.jpg')"
-      >
-        <template #title>
-          <div>
-            <KTextTruncator
-              text="The Philosophy of Rick and Morty and the meaning of All life The Philosophy of Rick and Morty and the meaning of All life"
-              :maxLines="3"
-            />
-          </div>
-        </template>
-        <template #aboveTitle>
-          <div>
-            Rutrum pellentesque utrum...
-          </div>
-        </template>
-        <template #footer>
-          <div>
-            <div class="footer-top-header">
-              <span>
-                <progress class="" value="50" max="100"></progress>
-              </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
-              </span>
-            </div>
-          </div>
-        </template>
-      </KCard>
-
-      <br>
+    <ul style="width:40%">
       <KCard
         :to="{ name: '/' }"
         :headingLevel="2"
@@ -251,37 +231,82 @@
         <template #title>
           <div>
             <KTextTruncator
-              text="The Philosophy of Rick and Morty and the meaning of All life"
+              text="This is a sample resource title that goes to three lines and truncates at the end of the three lines This is a sample title that goes"
               :maxLines="2"
             />
           </div>
         </template>
         <template #aboveTitle>
-          <div>
-            Rutrum pellentesque utrum...
-          </div>
+          <KGrid>
+
+            <KGridItem
+              :layout12="{ span: 8 }"
+              :layout8="{ span: 6 }"
+            >
+              <span><KIcon icon="readSolid" class="icon-size" /></span>
+              <span
+                :style="{ 'color': $themePalette.grey.v_400,
+                          'font-size': '1.2em' }"
+              >Rutrum pellentesque utrum...</span>
+            </KGridItem>
+
+            <KGridItem
+              :layout12="{ span: 4 }"
+              :layout8="{ span: 2 }"
+            >
+              <div style="float:right">
+                <KIcon icon="infoOutline" class="icon-size" />
+              </div>
+            </KGridItem>
+          </KGrid>
         </template>
         <template #footer>
           <div>
             <div class="footer-top-header">
-              <span> <KIcon icon="optionsVertical" /> Practice</span>
-              <span>Short Activity</span>
-            </div>
-            <br>
-            <div class="footer-top-header">
-              <span>
-                <progress class="" value="50" max="100"></progress>
+              <span
+                :style="{ 'background-color': $themePalette.grey.v_100 }"
+              >
+                <KIcon icon="practiceSolid" />
+                Practice
               </span>
-              <span>
-                <KIcon icon="schedule" class="icon-size" />
-              </span>
-              <span>
-                <KIcon icon="download" class="icon-size" />
+              <span
+                :style="{ 'background-color': $themePalette.grey.v_100 }"
+              >
+                Short Activity
               </span>
             </div>
+
+            <KGrid>
+              <KGridItem
+                :layout12="{ span: 2 }"
+                :layout8="{ span: 2 }"
+              >
+                <div class="">
+                  <KIcon icon="schedule" class="icon-size" />
+                </div>
+              </KGridItem>
+              <KGridItem
+                :layout12="{ span: 8 }"
+                :layout8="{ span: 6 }"
+              >
+                <progress
+                  value="20"
+                  max="100"
+                  style="height:0.5em;width:100%"
+                ></progress>
+              </KGridItem>
+              <KGridItem
+                :layout12="{ span: 2 }"
+                :layout8="{ span: 2 }"
+              >
+                <div class="" style="float:right;">
+                  <KIcon icon="download" class="icon-size" />
+                </div>
+              </KGridItem>
+            </KGrid>
           </div>
         </template>
-      </KCard>
+      </kcard>
     </ul>
   </div>
 
@@ -311,14 +336,21 @@
 
 <style scoped>
 .footer-top-header{
-  display: inline-flex;
+  display: flex;
   font-size: 14px;
+  margin-top: 1em;
+  margin-bottom: 0.5em;
 }
 
 .footer-top-header span{
-  margin-right: 20px;
+  margin-right: 30px;
 }
 .icon-size{
   font-size:20px;
 }
+.icon{
+  font-size:20px;
+  margin-left: 300px;
+}
+
 </style>
