2e713bccf1a3f395bf43a7d8cfb155d3bb7991ca
Kshitij Thareja <kshitijthareja03@gmail.com>
2024-08-07 17:05:06 +0530

    Remove .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++
@@ -1,8 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  cacheDirectory: process.env.CI ? join(__dirname, '.cache', 'puppeteer') : undefined,
-};
