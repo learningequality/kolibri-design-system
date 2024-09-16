bcb8bad2712c8640673a7fc9f246d5536b4be7f6
Michaela Robosova <robosovam@gmail.com>
2024-09-16 23:07:12 +0200

    Move playground to a directory
    to have a suitable location
    for playground sub-components
    when testing complex scenarios

---
+++ docs/pages/playground/index.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <!--
+    Playground: A private page for components development
+    *****************************************************
+    Place a component you're working on here and see it
+    live on http://localhost:4000/playground
+
+    Please do not commit your local updates of this file.
+  -->
+  <div style="padding: 24px">
+    <!-- 
+      Example: Uncomment lines bellow, change something
+      in lib/KBreadcrumbs.vue and see the updates reflected
+      on this page
+    -->
+    <!-- <KBreadcrumbs
+      :items="[
+        { text: 'Global Digital Library', link: { path: '#' } },
+        { text: 'English', link: { path: '#' } },
+        { text: 'Level 2 ', link: { path: '#' } },
+      ]"
+    /> -->
+
+    <!-- Play around with your component here: -->
+
+  </div>
+
+</template>
+
+
+<script>
+
+  /*
+    Playground is a Vue component too,
+    so you can also use `data`, `methods`, etc.
+    as usual if helpful
+  */
+  export default {
+    name: 'Playground',
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+
+</script>
