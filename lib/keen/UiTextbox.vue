0d4542f67781c401fdc520b51b8812687a0ad5af
shruti862 <shrutiyadav862@gmail.com>
2025-03-05 01:31:15 +0530

    new commit

--- lib/keen/UiTextbox.vue
+++ lib/keen/UiTextbox.vue
@@ -98,7 +98,11 @@
       </label>
 
       <div class="ui-textbox-feedback">
-        <div v-if="showError" class="ui-textbox-feedback-text" :style="{ color: $themeTokens.error }">
+        <div 
+          v-if="showError" 
+          class="ui-textbox-feedback-text" 
+          :style="{ color: $themeTokens.error }"
+        >
           <slot name="error">
             {{ error }}
           </slot>
