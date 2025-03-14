8dfb811d758f4111273aa35d1c4c9a457ea03654
GautamBytes <manchandanigautam@gmail.com>
2025-03-14 17:05:06 +0000

    added visual tests for kdropdownmenu

--- jest.conf/visual.load-test-components.js
+++ jest.conf/visual.load-test-components.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 
 import KButtonWithDropdownTest from '~~/lib/buttons-and-links/__tests__/components/KButtonWithDropdownTest.vue';
+import KDropdownMenuTest from '~~/lib/KDropdownMenu/__tests__/components/KDropdownMenuTest.vue';
 
 Vue.component('KButtonWithDropdownTest', KButtonWithDropdownTest);
+Vue.component('KDropdownMenuTest', KDropdownMenuTest);
+
---
+++ lib/KDropdownmenu/__tests__/components/KDropdownMenuTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="kdropdown-test-container">
+    <!-- KDropdownMenu with header slot -->
+    <div class="test-case">
+      <KButton text="Dropdown with Header">
+        <template #menu>
+          <KDropdownMenu :options="['Option 1', 'Option 2']">
+            <template #header>
+              <h3 style="padding: 8px 16px; margin: 0">Menu Header</h3>
+            </template>
+          </KDropdownMenu>
+        </template>
+      </KButton>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KDropdownMenuTest',
+};
+</script>
+
+<style scoped>
+.kdropdown-test-container {
+  padding: 20px;
+}
+</style>
