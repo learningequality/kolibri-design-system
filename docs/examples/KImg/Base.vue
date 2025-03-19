957c26fef7694e80d6630b0824ba1502c95b37d0
GautamBytes <manchandanigautam@gmail.com>
2025-03-19 06:49:52 +0000

    Reset unrelated files to match develop branch

--- docs/examples/KImg/Base.vue
+++ docs/examples/KImg/Base.vue
@@ -1,8 +1,8 @@
 <template>
 
-    <KImg
-      :src="require('../../assets/hummingbird-small-cc-by-sa-4.jpg')"
-      altText="A sitting hummingbird"
-    />
-  
-  </template>
+  <KImg
+    :src="require('../../assets/hummingbird-small-cc-by-sa-4.jpg')"
+    altText="A sitting hummingbird"
+  />
+
+</template>
