f105651b1033b90926c9924611adc15100c3eedf
Jacob Pierce <w.jacobpierce@gmail.com>
2025-05-20 13:41:04 -0700

    Add coach assignment icon, run generator

---
+++ lib/KIcon/precompiled-icons/material-icons/arrow_circle_left/twotone.vue
@@ -0,0 +1,12 @@
+<template>
+
+  <svg viewBox="0 0 24 24" role="presentation" focusable="false" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path opacity=".3" d="M20 12c0 4.41-3.59 8-8 8s-8-3.59-8-8 3.59-8 8-8 8 3.59 8 8m-8 1h4v-2h-4V8l-4 4 4 4v-3z"/><path d="M20 12c0 4.41-3.59 8-8 8s-8-3.59-8-8 3.59-8 8-8 8 3.59 8 8m2 0c0-5.52-4.48-10-10-10S2 6.48 2 12s4.48 10 10 10 10-4.48 10-10zm-10 1h4v-2h-4V8l-4 4 4 4v-3z"/></svg>
+
+</template>
+
+
+<script>
+
+  export default {"name":"icon-b9f2ee12c6ca30eccf0c30e4d4e1e93d"}
+
+</script>
