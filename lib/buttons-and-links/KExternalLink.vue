ab22b2f5d1592901f4a5a953f18ca7159fb0e8c5
Tushar Verma <tusharmyself06@gmail.com>
2025-07-29 16:25:03 +0530

    fix(KExternalLink): apply feedback — wrap with inner span for flex, add @slot comments, fix RTL

--- lib/buttons-and-links/KExternalLink.vue
+++ lib/buttons-and-links/KExternalLink.vue
@@ -12,39 +12,40 @@
     @mouseenter="hovering = true"
     @mouseleave="hovering = false"
   >
-    <!-- @slot Slot alternative to the `icon` prop -->
-    <slot name="icon">
+    <span class="kexlink-inner">
+      <!-- @slot Icon before text. Slot alternative to the `icon` prop. -->
+      <slot name="icon">
+        <KIcon
+          v-if="icon"
+          :icon="icon"
+          :color="iconColor"
+        />
+      </slot>
+
+      <!-- @slot Link text -->
+      <slot v-if="$slots.default"></slot>
+
+      <template v-else>
+        <span
+          class="link-text"
+          :style="spanStyle"
+        >{{ text }}</span>
+      </template>
+
+      <!-- @slot Icon after text. Slot alternative to the `iconAfter` prop. -->
+      <slot name="iconAfter">
+        <KIcon
+          v-if="iconAfter"
+          :icon="iconAfter"
+          :color="iconColor"
+        />
+      </slot>
       <KIcon
-        v-if="icon"
-        :icon="icon"
-        style="top: 4px"
+        v-if="openInNewTab"
+        icon="openNewTab"
         :color="iconColor"
       />
-    </slot>
-
-    <slot v-if="$slots.default"></slot>
-
-    <template v-else>
-      <span
-        class="link-text"
-        :style="spanStyle"
-      >{{ text }}</span>
-    </template>
-
-    <slot name="iconAfter">
-      <KIcon
-        v-if="iconAfter"
-        :icon="iconAfter"
-        style="top: 4px"
-        :color="iconColor"
-      />
-    </slot>
-    <KIcon
-      v-if="openInNewTab"
-      icon="openNewTab"
-      style="top: 4px"
-      :color="iconColor"
-    />
+    </span>
   </a>
 
 </template>
@@ -102,37 +103,6 @@
         hovering: false,
       };
     },
-    computed: {
-      /**
-       * If link opens in new tab or if icon is provided,
-       * add 8px margin between the icon and the text
-       */
-      spanStyle() {
-        const styles = {};
-        if (this.openInNewTab || this.icon) {
-          if (this.isRtl) {
-            // If RTL-language, but English link, displays correct margins
-            styles['marginRight'] = '8px';
-            // Checks to see if link for new tab is in same dir as page lang
-            if (this.text !== this.href) {
-              styles['marginRight'] = '0px';
-              styles['marginLeft'] = '8px';
-            }
-          } else {
-            if (this.text === this.href) {
-              styles['marginRight'] = '8px';
-            } else {
-              styles['marginLeft'] = '8px';
-            }
-          }
-        }
-
-        if (this.iconAfter) {
-          styles['marginRight'] = '8px';
-        }
-        return { ...styles };
-      },
-    },
   };
 
 </script>
@@ -141,5 +111,11 @@
 <style lang="scss" scoped>
 
   @import './buttons';
+
+  .kexlink-inner {
+    display: inline-flex;
+    gap: 8px;
+    align-items: center;
+  }
 
 </style>
