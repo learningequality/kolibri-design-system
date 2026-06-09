8386fd64662b021a0f06f55d50a1db147f6ceb30
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2026-06-09 20:17:53 +0530

    feat: add color prop to KCheckbox to customize checked and indeterminate icon states
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

---
+++ examples/KCheckbox/CustomColor.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div>
+    <KCheckbox
+      v-model="inputValue"
+      label="Selected with custom color"
+      color="#4caf50"
+    />
+    <KCheckbox
+      v-model="indeterminate"
+      label="Indeterminate with custom color"
+      :indeterminate="true"
+      color="#4caf50"
+    />
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    data() {
+      return {
+        inputValue: true,
+        indeterminate: false,
+      };
+    },
+  };
+
+</script>
