c44b7f630097292da0e7740ac31c2af16820954a
rtibblesbot <richard+githubagent@learningequality.org>
2026-07-03 08:01:41 -0700

    Consolidate KImg visual tests into a single KImgVisualTest.vue
    Replace 9 per-scenario snapshots with a single takeSnapshot call to
    avoid hitting Percy snapshot limits (learningequality/kolibri-design-system#1059).

---
+++ examples/KImg/WithBorder.vue
@@ -0,0 +1,9 @@
+<template>
+
+  <KImg
+    :src="require('../common/hummingbird-small-cc-by-sa-4.jpg')"
+    altText="A sitting hummingbird"
+    borderRadius="8px"
+  />
+
+</template>
---
+++ lib/KImg/__tests__/components/KImgVisualTest.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <VisualTestLayout>
+    <VisualTestExample
+      title="Base"
+      loadExample="KImg/Base.vue"
+    />
+
+    <VisualTestExample
+      title="Aspect Ratio"
+      loadExample="KImg/AspectRatio.vue"
+    />
+
+    <VisualTestExample
+      title="Content On Top"
+      loadExample="KImg/ContentOnTop.vue"
+    />
+
+    <VisualTestExample
+      title="Placeholder"
+      loadExample="KImg/Placeholder.vue"
+    />
+
+    <VisualTestExample
+      title="Fit XY"
+      loadExample="KImg/FitXY.vue"
+    />
+
+    <VisualTestExample
+      title="Fit Contain"
+      loadExample="KImg/FitContain.vue"
+    />
+
+    <VisualTestExample
+      title="Fit Center Inside"
+      loadExample="KImg/FitCenterInside.vue"
+    />
+
+    <VisualTestExample
+      title="With Border"
+      loadExample="KImg/WithBorder.vue"
+    />
+
+    <VisualTestExample
+      title="With Background Color"
+      loadExample="KImg/WithBackgroundColor.vue"
+    />
+  </VisualTestLayout>
+
+</template>
