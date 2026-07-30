f35f4bf357a597aa308a2425fe086e27b10bdf22
Prashant-thakur77 <prashant101007@gmail.com>
2026-07-30 22:27:47 +0530

    Added the visualsuite for kmutliselect

---
+++ lib/candidate/multiselect/KMultiSelect/__tests__/components/KMultiSelectVisualTest.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <VisualTestLayout>
+    <VisualTestExample
+      title="Multiple selection"
+      width="800px"
+      loadExample="KMultiSelect/MultipleSelection.vue"
+    />
+    <VisualTestExample
+      title="Single selection"
+      width="400px"
+      loadExample="KMultiSelect/SingleSelection.vue"
+    />
+    <VisualTestExample
+      title="Primitive values"
+      width="400px"
+      loadExample="KMultiSelect/PrimitiveValues.vue"
+    />
+    <VisualTestExample
+      title="Multi-field search"
+      width="400px"
+      loadExample="KMultiSelect/MultifieldSearch.vue"
+    />
+    <VisualTestExample
+      title="Hierarchical tree"
+      width="400px"
+      loadExample="KMultiSelect/HierarchicalTree.vue"
+    />
+  </VisualTestLayout>
+
+</template>
